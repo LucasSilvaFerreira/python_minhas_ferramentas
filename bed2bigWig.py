a79b1795692b47f169725f103e18fa04b2572e33
bioinfolucas@gmail.com <@koboldd12>
2015-11-17 18:20:00 -0200

    Initial commit

---
+++ bed2bigWig.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import argparse
+
+def main():
+    '''Parte principal do programa onde as outras funcoes sao chamadas'''
+    obj = argparse.ArgumentParser(description="Convert bam files in BigWig files. Can be used in multiples files"
+                                              "separeted by space")
+    obj.add_argument("-bam", nargs='+', help="Bam file path (Create a index before run)", required=True)
+    obj.add_argument("-chrS", help="chrSize file sorted by number", required=True)
+    arg = obj.parse_args()
+
+    for bam_in in arg.bam:
+        print 'Converting: {}'.format(bam_in)
+        convert_bam_files(bam_in, arg.chrS)
+
+
+
+def generate_index(bam_file):
+    print 'Genrating index bam..'
+    generate_bai_cmd = 'bamtools index  -in {}'.format(bam_file)
+    print generate_bai_cmd
+    os.system(generate_bai_cmd)
+
+
+def convert_bam_files(bam_file, genome_size_file):
+        genome_size_file = os.path.abspath(genome_size_file)
+        possible_bai = bam_file.rsplit(".", 1)[0] + ".bai"
+        out_cov = bam_file.rsplit(".", 1)[0] + ".cov"
+        out_bw = bam_file.rsplit(".", 1)[0] + ".bw"
+
+        if not os.path.exists(possible_bai):
+            generate_index(bam_file)
+
+        genome_coverage_cmd = "genomeCoverageBed -ibam {} -bg -split -g {} > {}".format(bam_file,
+
+                                                                                               genome_size_file,
+                                                                                                   out_cov)
+        print genome_coverage_cmd
+        os.system(genome_coverage_cmd)
+        #print genome_coverage_cmd
+
+        bed_graph_to_bw_cmd =  "bedGraphToBigWig  {} {}  {}".format(out_cov, genome_size_file, out_bw)
+        print bed_graph_to_bw_cmd
+        os.system(bed_graph_to_bw_cmd)
+        #print bed_graph_to_bw_cmd
+
+
+if __name__ == '__main__':
+    sys.exit(main())
